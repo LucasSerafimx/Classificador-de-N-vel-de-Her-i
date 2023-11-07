95afeab699a94c004bf03d2272ac3185f257028a
LucasSerafimx <lucas.serafimx@gmail.com>
2023-11-06 23:44:17 -0300

    Primeiro Desafio Dio Me

---
+++ idex.js
@@ -0,0 +1,27 @@
+let nomeDoHeroi = "Serafins"
+let xpDoHeroi = 10000
+
+if(xpDoHeroi <= 1000){
+    console.log("O Herói de nome " + nomeDoHeroi + " Está no nivel de: Ferro")
+
+}else if (xpDoHeroi <= 2000){
+    console.log("O Herói de nome " + nomeDoHeroi + " Está no nivel de: Bronze")
+
+}else if (xpDoHeroi <= 5000){
+    console.log("O Herói de nome " + nomeDoHeroi + " Está no nivel de: Prata")
+
+}else if (xpDoHeroi <= 6000){
+    console.log("O Herói de nome " + nomeDoHeroi + " Está no nivel de: Ouro")
+
+}else if (xpDoHeroi <= 8000){
+    console.log("O Herói de nome " + nomeDoHeroi + " Está no nivel de: Platina")
+
+}else if (xpDoHeroi <= 9000){
+    console.log("O Herói de nome " + nomeDoHeroi + " Está no nivel de: Ascendente")
+
+}else if (xpDoHeroi <= 10000){
+    console.log("O Herói de nome " + nomeDoHeroi + " Está no nivel de: Imortal")
+
+}else {
+    console.log("O Herói de nome " + nomeDoHeroi + " Está no nivel de: Radiante")
+}
